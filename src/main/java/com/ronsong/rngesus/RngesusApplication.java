9cfa936e5a359d37a89ae49c0d8af9c609b2b8ad
Ronnie Song <ronniesong0809@gmail.com>
2021-02-20 23:54:37 -0800

    init

---
+++ src/main/java/com/ronsong/rngesus/RngesusApplication.java
@@ -0,0 +1,13 @@
+package com.ronsong.rngesus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RngesusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RngesusApplication.class, args);
+    }
+
+}
