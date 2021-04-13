5be1dfff3aab731698a27365b5093b957d61351d
Ronnie Song <ronniesong0809@gmail.com>
2021-04-12 23:22:18 -0700

    add mybatis plus pagination plugin

---
+++ src/main/java/com/ronsong/rngesus/common/plugin/MybatisPlusPlugins.java
@@ -0,0 +1,17 @@
+package com.ronsong.rngesus.common.plugin;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusPlugins {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        return interceptor;
+    }
+}
