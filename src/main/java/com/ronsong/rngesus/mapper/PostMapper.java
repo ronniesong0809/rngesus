7a82cbd287ed349e0eb904f519ebcc2131ca6e09
Ronnie Song <ronniesong0809@gmail.com>
2021-04-12 21:17:18 -0700

    refactor sql to sort the result set in diff order

--- src/main/java/com/ronsong/rngesus/mapper/PostMapper.java
+++ src/main/java/com/ronsong/rngesus/mapper/PostMapper.java
@@ -13,8 +13,17 @@ public interface PostMapper extends BaseMapper<Post> {
     @Select("<script>" +
             "SELECT p.id, p.title, p.comments, p.view, p.create_time, p.modify_time, u.id, u.username, u.alias, u.avatar\n" +
             "FROM post p LEFT JOIN users u ON p.user_id = u.id" +
-            "<where></where>" +
+            "<where>" +
+            "<if test=\"tab == 'hottest'\">"+
+            "date(p.create_time) &gt;= NOW() - INTERVAL '1 month'" +
+            "</if>" +
+            "</where>" +
+            "<if test=\"tab == 'hottest'\">"+
+            "ORDER BY p.view desc, p.create_time DESC" +
+            "</if>" +
+            "<if test=\"tab != 'hottest'\">"+
             "ORDER BY p.create_time DESC" +
+            "</if>" +
             "</script>")
     Page<PostVO> selectListAndPage(@Param("page") Page<PostVO> page, @Param("tab") String tab);
 }
